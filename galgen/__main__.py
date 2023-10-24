d07b2f1392ac860c6cf0259b50e5634728b11ee8
Grzegorz Krason <grzegorz.krason@gmail.com>
2023-10-24 15:40:00 +0200

    add logging

--- galgen/__main__.py
+++ galgen/__main__.py
@@ -1,6 +1,13 @@
 import click
+import coloredlogs
+from galgen import UserError
 from galgen.init import init_action
 from galgen.build import build_action
+from logging import getLogger
+
+coloredlogs.install(level='INFO', fmt='%(message)s')
+
+logger = getLogger(__file__)
 
 @click.group
 def main():
@@ -12,7 +19,10 @@ def main():
 @click.option('-f', '--force', default=False, is_flag=True, help='Overwrite existing files')
 def init(path, force):
     """Generate config files."""
-    init_action(path, force)
+    try:
+        init_action(path, force)
+    except UserError as exc:
+        logger.critical(str(exc))
 
 @main.command()
 @click.argument('path', type=click.Path(exists=True, file_okay=False, dir_okay=True))
@@ -20,7 +30,10 @@ def init(path, force):
 @click.option('-f', '--force', default=False, is_flag=True, help='Overwrite existing files')
 def build(path, force, show):
     """Generate webpage."""
-    build_action(path, force, show)
+    try:
+        build_action(path, force, show)
+    except UserError as exc:
+        logger.critical(str(exc))
 
 if __name__ == '__main__':
     main()
--- galgen/init.py
+++ galgen/init.py
@@ -2,7 +2,10 @@ import yaml
 import shutil
 import pkg_resources
 from pathlib import Path
-from galgen import CONFIG_NAME
+from galgen import UserError, CONFIG_NAME
+from logging import getLogger
+
+logger = getLogger(__file__)
 
 def init_action(path, force):
     path = Path(path)
@@ -11,7 +14,7 @@ def init_action(path, force):
                  for f in sorted(path.iterdir()) if f.is_dir()]
 
     if not len(galleries):
-        raise RuntimeError(f'At least one directory expected in {path}')
+        raise UserError(f'At least one directory expected in: {path}')
     elif len(galleries) == 1:
         galleries[0]['title'] = ''
     
@@ -22,9 +25,11 @@ def init_action(path, force):
     template_dst_path = path / Path(template_src_path).name
         
     if not force and (config_path.exists() or template_dst_path.exists()):
-        raise RuntimeError('Gallery already initialized. Use -f to overwrite.')
+        raise UserError('Gallery already initialized. Use -f to overwrite.')
     
     with config_path.open('w') as f:
         f.write(yaml.dump(config, sort_keys=False))
+    logger.info(f'File generated: {config_path}')
     
     shutil.copyfile(template_src_path, template_dst_path)
+    logger.info(f'File generated: {template_dst_path}')
